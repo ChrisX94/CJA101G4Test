6c7617112373e0e6ddb15dbc93ec159981293ba2
Chris Xiao <xchris80519@gmail.com>
2025-05-15 10:39:40 +0800

    first commit

---
+++ src/main/java/mall/model/ProductVO.java
@@ -0,0 +1,26 @@
+package mall.model;
+
+public class ProductVO {
+	
+	private int prod_id ;
+	private String prod;
+	
+	public ProductVO() {
+		super();
+	}
+	
+	public int getProd_id() {
+		return prod_id;
+	}
+	public void setProd_id(int prod_id) {
+		this.prod_id = prod_id;
+	}
+	public String getProd() {
+		return prod;
+	}
+	public void setProd(String prod) {
+		this.prod = prod;
+	}
+
+
+}
