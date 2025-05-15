643798e4b30f57906ea14e3808391b0a48fe7356
Carter <whiteslime891201@gmail.com>
2025-05-15 11:53:44 +0800

    change!

--- src/main/java/com/g4/service/model/HelloGit.java
+++ src/main/java/com/g4/service/model/HelloGit.java
@@ -1,9 +1,20 @@
 package com.g4.service.model;
 
+import java.security.DrbgParameters.NextBytes;
+import java.util.Scanner;
+
 public class HelloGit {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String c = sc.next();
 		String h = "Hello Git!";
-		System.out.println(h);
+		String y = "pass!";
+		if(c == h) {
+			System.out.println(h);
+		}else {
+			System.out.println(y);
+		}
+		
 	}
 	
 
